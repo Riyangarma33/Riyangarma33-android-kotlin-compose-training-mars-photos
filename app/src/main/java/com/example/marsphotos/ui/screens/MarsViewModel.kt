4832ce215bd74bbe7ae27b6afcd2b7dca3928e15
riyangarma33 <mgarma76@gmail.com>
2024-05-16 13:38:03 +0700

    Show Image to Home Screen

--- app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -35,7 +35,8 @@ import java.io.IOException
  * UI state for the Home screen
  */
 sealed interface MarsUiState {
-    data class Success(val photos: String) : MarsUiState
+//    data class Success(val photos: String) : MarsUiState
+    data class Success(val photos: List<MarsPhoto>): MarsUiState
     object Error : MarsUiState
     object Loading : MarsUiState
 }
@@ -60,10 +61,11 @@ class MarsViewModel(private val marsPhotosRepository: MarsPhotosRepository) : Vi
         viewModelScope.launch {
             marsUiState = MarsUiState.Loading
             marsUiState = try {
-                val listResult = marsPhotosRepository.getMarsPhotos()
-                MarsUiState.Success(
-                    "Success: ${listResult.size} Mars photos retrieved"
-                )
+//                val listResult = marsPhotosRepository.getMarsPhotos()
+//                MarsUiState.Success(
+//                    "Success: ${listResult.size} Mars photos retrieved"
+//                )
+                MarsUiState.Success(marsPhotosRepository.getMarsPhotos())
             } catch (e: IOException) {
                 MarsUiState.Error
             } catch (e: HttpException) {
